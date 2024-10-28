3774ce1db18239dc570c8627274cd98edfca29c9
MehmetBari <mbarievski@gmail.com>
2024-10-28 16:12:02 +0100

    Börjat på CardioWorkout

---
+++ Classes/CardioWorkout.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FITTRACK_PROJEKTUPPGIFT_OPG.Classes
+{
+    public class CardioWorkout : Workout 
+    {
+        //Egenskaper
+        public int Distance { get; set; }
+
+        //Konstruktor
+
+        public Workout(int Distance)
+        {
+            this.Distance = Distance;
+        }
+        //Metod 
+        public override int CalculateCaloriesBurned()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
