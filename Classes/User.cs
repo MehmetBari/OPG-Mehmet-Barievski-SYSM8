e87542c19b0bd0c0924d0cd2d37135e83a0de090
MehmetBari <mbarievski@gmail.com>
2024-10-28 14:42:59 +0100

    Börjat på User Klassen

---
+++ Classes/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FITTRACK_PROJEKTUPPGIFT_OPG.Classes
+{
+    public class User : Person 
+    {
+        //Egenskaper
+
+        public string Country { get; set; }
+
+        public string SecurityQuestion { get; set; }
+
+        public string SecurityAnswer { get; set; }
+
+        //Konstruktor
+
+        public User(string Country, string SecurityQuestion, string SecurityAnswer)
+        {
+            this.Country = Country;
+            this.SecurityQuestion = SecurityQuestion;
+            this.SecurityAnswer = SecurityAnswer;
+        }
+        // Metod override från Person
+        public override void SignIn()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
