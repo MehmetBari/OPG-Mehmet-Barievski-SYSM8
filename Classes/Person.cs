26ecebeedad64858fd6342b397fc2ca095c15df4
MehmetBari <mbarievski@gmail.com>
2024-10-28 14:21:43 +0100

    Person class klar

---
+++ Classes/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FITTRACK_PROJEKTUPPGIFT_OPG.Classes
+{
+    public abstract class Person
+    {
+        //Egenskaper
+        public string Username { get; set; }
+
+        public string Password { get; set; } 
+
+        //Konstruktor 
+
+        public Person(string username, string password)
+        {
+            this.Username = username;
+            this.Password = password;
+        }
+
+        //Metod
+        public abstract void SignIn();
+
+    }
+}
