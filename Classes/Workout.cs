b80a0c1bcf9a59bb6e91ca0f93fc781ed7e0732b
MehmetBari <mbarievski@gmail.com>
2024-10-28 15:57:21 +0100

    Börjat på Workout klassen

---
+++ Classes/Workout.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FITTRACK_PROJEKTUPPGIFT_OPG.Classes
+{
+    public abstract class Workout
+    {
+        //Egenskaper
+
+        public DateTime Date { get; set; }
+
+        public string Type { get; set; }
+
+        public TimeSpan Duration { get; set; }
+
+        public int CaloriesBurned { get; set; }
+
+        public string Notes { get; set; } 
+
+        //Konstruktor 
+
+        public Workout(DateTime Date, string Type, string Notes, TimeSpan Duration, int CaloriesBurned)
+        {
+            this.Date = Date;
+            this.Type = Type;
+            this.Notes = Notes;
+            this.Duration = Duration;
+            this.CaloriesBurned = CaloriesBurned;
+        }
+        //Metod 
+
+        public abstract int CalculateCaloriesBurned();
+    }
+}
